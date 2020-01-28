dea49562fa0db6aa6a88a84202dd3937931790e3
mengxiang <1547719290@qq.com>
2020-01-28 13:40:52 +0800

    城市搜索完成

--- src/components/city/commponents/List.vue
+++ src/components/city/commponents/List.vue
@@ -7,24 +7,6 @@
             <div class="button-wrapper">
               <div class="button">北京</div>
             </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
           </div>
         </div>
         <div class="area">
