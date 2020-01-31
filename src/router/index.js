9cb18bdc0b1bd39415cab3ad407e2ab917a1617b
mengxiang <1547719290@qq.com>
2020-01-31 10:00:28 +0800

    添加detail页面的路由；修改home路由为Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/home/home'
+import Home from '@/components/home/Home'
 import City from '@/components/city/City'
+import Detail from '@/components/detail/Detail'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
