9d6785503b6bcb64d838c2ef893d301a4838c1b3
mengxiang <1547719290@qq.com>
2020-01-31 12:06:10 +0800

    画廊

---
+++ src/components/detail/components/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+      <div class="banner" @click="handleBannerClick">
+          <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg">
+          <div class="banner-info">
+            <div class="banner-title">
+              大连圣亚海洋世界(AAAA景区)
+            </div>
+            <div class="banner-number">
+              <span class="iconfont banner-icon">&#xe7b3;</span>
+              39
+            </div>
+          </div>
+      </div>
+      <common-gallary @close="handleGallaryClick" :imgs="imgs" v-show="showGallary"></common-gallary>
+    </div>
+</template>
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data() {
+    return {
+      showGallary: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1510/8e/8eea8eb6f41698290.img.jpg_r_800x800_83a5fe3a.jpg',
+        'http://img1.qunarzz.com/sight/p0/1510/ca/ca60a080020329ef90.img.jpg_350x240_9ff2208e.jpg',
+        'http://img1.qunarzz.com/sight/p0/1510/cc/ccafbdaac21bdbf790.img.jpg_350x240_c35f8451.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallary = true
+    },
+    handleGallaryClick() {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .banner
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    position: relative
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #ffffff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
